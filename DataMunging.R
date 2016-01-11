bddfdbdfb66d3b7a007dddca221635c251960444
ryanlawson <ryan.lawson2@gmail.com>
2016-01-11 11:57:45 -0500

    More Duplicate Removal
    Line 12: changed filename
    
    Lines 14-16: removed duplicate rows
    
    Lines 18-22: removed visuals, coerced names to lowercase
    
    Lines 25-27: changed mergedData (for diagnostics)

--- DataMunging.R
+++ DataMunging.R
@@ -9,34 +9,19 @@ setwd("~/R");
 
 # import datasets
 lcdata <- read.csv(file="StudentCompiled.csv", header=T)
-irdata <- read.csv(file="oire_stu_0915.csv", header=T)
+irdata <- read.csv(file="oire_student_0915.csv", header=T)
 
-# check for duplicates
-dupes = irdata[duplicated(irdata),]
-dupes[1,]
-length(dupes)
+# remove duplicate rows (if any)
+dupes <- irdata[duplicated(irdata),]
+irdata <- irdata[!duplicated(irdata),]
 
-
-# remove duplicate rows
-  #lcdata <- unique(lcdata)
-  #irdata <- unique(irdata)
-
-# check names and dimensions
-names(lcdata)
-names(irdata)
-dim(lcdata)
-dim(irdata)
-
-# visuals
-hist(irdata$SAT_MATH)
-hist(irdata$SAT_WRITING)
-hist(irdata$EXPECTED_FAMILY_CONTRIBUTION)
-hist(log(irdata$EXPECTED_FAMILY_CONTRIBUTION))
-hist(lcdata$SUM)
-hist(lcdata$SUM, col="lightblue")
-hist(log(lcdata$SUM), col="lightblue")
+# coerce names to lowercase for consistency
+irdata$FIRST_NAME <- tolower(irdata$FIRST_NAME)
+irdata$LAST_NAME <- tolower(irdata$LAST_NAME)
+lcdata$FIRST_NAME <- tolower(lcdata$FIRST_NAME)
+lcdata$LAST_NAME <- tolower(lcdata$LAST_NAME)
 
 # merge sets (left join)
-mergedData <- merge(irdata, lcdata, c("TERM_CODE", "COURSE_ACRONYM", "COURSE_NUMBER", 
-                                      "LAST_NAME", "FIRST_NAME"), all.x=T)
-
+mergedData <- merge(irdata, lcdata, by=c("TERM_CODE","COURSE_ACRONYM", 
+                                         "COURSE_NUMBER", "LAST_NAME", 
+                                         "FIRST_NAME"), all.y=T, sort=F)
