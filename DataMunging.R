ddccab95f23693c77efda350f827312993cd3016
austinmctier <austin.mctier@bobcats.gcsu.edu>
2016-05-03 07:22:19 -0400

    Update DataMunging.R

--- DataMunging.R
+++ DataMunging.R
@@ -37,6 +37,342 @@ dataData <- merge(sidata, lcdata, by=c("TERM_CODE","COURSE_ACRONYM",
                                        "SI_LEADER"), all.y=T, sort=F)
 
 mergedData <- merge(dataData, irdata, by=c("TERM_CODE","COURSE_ACRONYM", 
+                                           "COURSE_NUMBER",'COURSE_SEC_IDENTIFIER', "LAST_NAME", 
+                                           "FIRST_NAME"), all.y=T, sort=F)
+
+# clean out environment
+remove(irdata)
+remove(dataData)
+remove(lcdata)
+remove(sidata)
+
+names(mergedData)
+
+# drop unwanted variables
+names(mergedData)[names(mergedData) == 'income_househould_median'] = 'income_household_median'
+names(mergedData)[names(mergedData) == 'Total'] = 'TOTAL'
+
+mergedData$ACTIVITY =  0
+mergedData$ACTIVITY[mergedData$ACTIVITY_NCAA_IND == 1 | mergedData$ACTIVITY_GREEK_IND == 1 | mergedData$ACTIVITY_ACADM_IND == 1] = 1
+mergedData$IPEDS_RACE_CODE[mergedData$IPEDS_RACE_CODE %in% c('I','M','A','P','U','T','U','Z','N')] = 'OTHER'
+# create proportions for population variables
+
+select =  c(
+    'pop_over25','pop_over16','pop','landArea',
+    'GIVE_HRS',
+    'income_household_median',
+    'home_medianValue','pop_black',
+    'ACTIVITY','SUMMER_CNT',
+    'TERM_CODE',
+    'STU_INST_UID',
+    'COURSE_ACRONYM',
+    'COURSE_NUMBER',
+    'STUDENT_LEVEL_NBR',
+    'INST_TERM_HRS_ATTEMPTED',
+    'INST_CUM_HRS_ATTEMPTED',
+    'INST_CUM_GPA',
+    'HS_GPA',
+    'SAT_CRITICAL_READING',
+    'SAT_MATH',
+    'ACT_COMPOSITE',
+    'BRIDGE_IND',
+    'GENDER_CODE',
+    'IPEDS_RACE_CODE',
+    'NN_GRANT',
+    'NR_GRANT',
+    'LOAN',
+    'MAJOR_DESC',
+    'DEPENDENCY_CODE',
+    'STUDENT_OR_PARENT_AGI',
+    'FATHER_HIGHEST_GRADE_CODE',
+    'MOTHER_HIGHEST_GRADE_CODE',
+    'COURSE_ATTEMPTED_HRS',
+    'INST_COURSE_GRADE',
+    'SI_LEADER',
+    'TOTAL',
+    'MEDIAN',
+    'MEAN',
+    'MAJOR_IND',
+    'pop_over25_bachelors',
+    'pop_armedForces',
+    'pop_over25_HSGrad',
+    'AGE',
+    'MAJOR_CHANGES',
+    'TERM_ORD',
+    'INST_CUM_HRS_EARNED',
+    'pop',
+    'COURSE_SEC_IDENTIFIER'
+)
+
+mergedData <- mergedData[,names(mergedData) %in% select]
+
+# drop PHYS2211 and GEOL1121 from term 201208
+mergedData <- mergedData[(mergedData$TERM_CODE==201208 & 
+                              mergedData$COURSE_ACRONYM=='PHYS' & 
+                              mergedData$COURSE_NUMBER=='2211')==FALSE,]
+mergedData <- mergedData[(mergedData$TERM_CODE==201208 & 
+                              mergedData$COURSE_ACRONYM=='GEOL' & 
+                              mergedData$COURSE_NUMBER=='1121')==FALSE,]
+
+# drop grades other than A, B, C, D, F
+mergedData <- mergedData[which(mergedData$INST_COURSE_GRADE == 'A' | 
+                                   mergedData$INST_COURSE_GRADE == 'B' | 
+                                   mergedData$INST_COURSE_GRADE == 'C' | 
+                                   mergedData$INST_COURSE_GRADE == 'D' | 
+                                   mergedData$INST_COURSE_GRADE == 'F'),]
+
+# create categorical variables
+mergedData$COURSE_ACRONYM <- factor(mergedData$COURSE_ACRONYM, ordered=FALSE)
+mergedData$COURSE_NUMBER <- factor(mergedData$COURSE_NUMBER, ordered=FALSE)
+mergedData$BRIDGE_IND <- factor(mergedData$BRIDGE_IND, ordered=TRUE)
+mergedData$GENDER_CODE <- factor(mergedData$GENDER_CODE, ordered=TRUE)
+mergedData$IPEDS_RACE_CODE <- factor(mergedData$IPEDS_RACE_CODE, ordered=FALSE)
+mergedData$IPEDS_RACE_CODE = relevel(mergedData$IPEDS_RACE_CODE,ref='W')
+mergedData$DEPENDENCY_CODE <- factor(mergedData$DEPENDENCY_CODE, ordered=TRUE)
+mergedData$MAJOR_IND <- factor(mergedData$MAJOR_IND, ordered=FALSE)
+mergedData$MAJOR_IND <- relevel(mergedData$MAJOR_IND, ref="14")
+
+# change NA's in SI_LEADER to "NONE"
+#mergedData$SI_LEADER[is.na(mergedData$SI_LEADER)] <- "NONE"
+mergedData$SI_LEADER <- factor(mergedData$SI_LEADER, ordered=FALSE)
+#mergedData$SI_LEADER <- relevel(mergedData$SI_LEADER, ref="NONE")
+
+# treat nas as zeros
+mergedData$TOTAL[is.na(mergedData$TOTAL)] <- 0
+mergedData$MEAN[is.na(mergedData$MEAN)] <- 0
+mergedData$MEDIAN[is.na(mergedData$MEDIAN)] <- 0
+mergedData$MAJOR_IND[is.na(mergedData$MAJOR_IND)] <- 14
+
+# create ordinal variables
+mergedData$FATHER_HIGHEST_GRADE_CODE <- as.numeric(factor(mergedData$FATHER_HIGHEST_GRADE_CODE, 
+                                                          levels=c(NA,1,2,3,4), 
+                                                          ordered=TRUE))
+mergedData$MOTHER_HIGHEST_GRADE_CODE <- as.numeric(factor(mergedData$MOTHER_HIGHEST_GRADE_CODE, 
+                                                          levels=c(NA,1,2,3,4), 
+                                                          ordered=TRUE))
+mergedData$INST_COURSE_GRADE <- as.numeric(factor(mergedData$INST_COURSE_GRADE, 
+                                                  levels=c('F','D','C','B','A'), 
+                                                  ordered=TRUE))
+# create time series variable using TERM_ORD
+mergedData$TERM_ORD <- factor(mergedData$TERM_ORD, ordered=T)
+mergedData$TERM_ORD <- ts(mergedData$TERM_ORD)
+
+ids = unique(mergedData$STU_INST_UID)
+terms = unique(mergedData$TERM_CODE)
+
+#sorted = sort(unique(mergedData$TERM_CODE[mergedData$STU_INST_UID == id]),descending=TRUE)
+#max = sorted[1]
+#almost_max = sorted[2]
+
+for(id in ids)
+{
+    terms = sort(unique(mergedData$TERM_CODE[mergedData$STU_INST_UID == id]))
+    if(length(terms) > 1)
+    {
+        for(i in 2:length(terms))
+        {
+            if(nrow( mergedData[mergedData$TERM_CODE == terms[i] & mergedData$STU_INST_UID == id,]) > 0 & 
+               nrow( mergedData[mergedData$TERM_CODE == terms[i-1] & mergedData$STU_INST_UID == id,]) > 0
+            ) {
+                mergedData$INST_CUM_GPA[mergedData$TERM_CODE == terms[i] & mergedData$STU_INST_UID == id] = 
+                    unique(mergedData$INST_CUM_GPA[mergedData$TERM_CODE == terms[i-1] & mergedData$STU_INST_UID == id])
+                
+                mergedData$INST_CUM_HRS_ATTEMPTED[mergedData$TERM_CODE == terms[i] & mergedData$STU_INST_UID == id] = 
+                    unique(mergedData$INST_CUM_HRS_ATTEMPTED[mergedData$TERM_CODE == terms[i-1] & mergedData$STU_INST_UID == id])
+                
+                mergedData$INST_CUM_HRS_EARNED[mergedData$TERM_CODE == terms[i] & mergedData$STU_INST_UID == id] = 
+                    unique(mergedData$INST_CUM_HRS_EARNED[mergedData$TERM_CODE == terms[i-1] & mergedData$STU_INST_UID == id])
+            }
+        }
+    }
+}
+
+
+mergedData <- mergedData[!is.na(mergedData$SI_LEADER),]
+
+
+
+# create treatment variable
+
+mergedData$treatment = 0
+mergedData$treatment[mergedData$TOTAL > 0] = 1
+
+# create class average variable
+# create unique ids for sections/SI Leaders
+mergedData$class_uniq_id = NA
+for(i in 1:nrow(mergedData))
+    mergedData[i,'class_uniq_id'] = paste(mergedData[i,'COURSE_SEC_IDENTIFIER'],
+                                          mergedData[i,'SI_LEADER'])
+# get unique ids from dataset
+classes_uniq_ids <- unique(mergedData$class_uniq_id)
+
+# shift course grade to 4-point scale
+mergedData$INST_COURSE_GRADE <- mergedData$INST_COURSE_GRADE - 1
+
+# create vector of class averages
+mergedData$class_average = NA
+for(class_uniq_id in classes_uniq_ids)
+    mergedData$class_average[mergedData$class_uniq_id == class_uniq_id] = 
+    mean(mergedData$INST_COURSE_GRADE[mergedData$class_uniq_id == class_uniq_id])
+
+# convert factors to numeric
+mergedData$BRIDGE_IND <- as.numeric(mergedData$BRIDGE_IND)
+mergedData$GENDER_CODE <- as.numeric(mergedData$GENDER_CODE)
+mergedData$IPEDS_RACE_CODE <- as.character(mergedData$IPEDS_RACE_CODE)
+mergedData$DEPENDENCY_CODE <- as.numeric(mergedData$DEPENDENCY_CODE)
+
+mergedData$income_household_median = as.numeric(mergedData$income_household_median)
+mergedData$home_medianValue = as.numeric(mergedData$home_medianValue)
+
+# run Amelia
+imputedData <- amelia(
+    x=mergedData,
+    m=1,
+    logs=c(
+        'STUDENT_OR_PARENT_AGI',
+        'income_household_median',
+        'home_medianValue','pop','landArea','pop_over25','pop_over16'
+    ),
+    sqrts=c(
+        'INST_CUM_HRS_ATTEMPTED',
+        'INST_CUM_HRS_EARNED'
+    ),
+    nom=c(
+        'BRIDGE_IND',
+        'GENDER_CODE',
+        'NR_GRANT',
+        'NN_GRANT',
+        'IPEDS_RACE_CODE',
+        'DEPENDENCY_CODE',
+        'ACTIVITY'
+    ),
+    ords=c(
+        'FATHER_HIGHEST_GRADE_CODE',
+        'MOTHER_HIGHEST_GRADE_CODE',
+        'INST_COURSE_GRADE'
+    ),
+    idvars=c(
+        'MAJOR_DESC',
+        'TERM_CODE',
+        'TERM_ORD',
+        'COURSE_ACRONYM',
+        'COURSE_NUMBER',
+        'COURSE_SEC_IDENTIFIER',
+        'treatment',
+        'class_uniq_id',
+        'STU_INST_UID',
+        'SI_LEADER',
+        'MAJOR_IND'
+    )
+)
+
+#get first imputation
+mergedData <- imputedData$imputations[[1]]
+
+
+#some data adjustments
+mergedData$COURSE_ACRONYM = as.character(mergedData$COURSE_ACRONYM)
+mergedData$IPEDS_RACE_CODE <- factor(mergedData$IPEDS_RACE_CODE, ordered=FALSE)
+mergedData$IPEDS_RACE_CODE = relevel(mergedData$IPEDS_RACE_CODE,ref='W')
+mergedData$MAJOR_IND <- factor(mergedData$MAJOR_IND, ordered=FALSE)
+mergedData$MAJOR_IND <- relevel(mergedData$MAJOR_IND, ref="14")
+
+# create proportions for population variables
+mergedData$pop_over25_HSGrad <- mergedData$pop_over25_HSGrad/mergedData$pop_over25
+mergedData$pop_over25_bachelors <- mergedData$pop_over25_bachelors/mergedData$pop_over25
+mergedData$pop_armedForces <- mergedData$pop_armedForces/mergedData$pop_over16
+mergedData$pop_black <- mergedData$pop_black/ (mergedData$pop)
+mergedData$pop <- mergedData$pop/mergedData$landArea*2.59e+6
+
+
+# create variable testing sameness of major and course department
+mergedData$MAJOR_COURSE_SAME = 0
+mergedData$MAJOR_COURSE_SAME[grepl('computer science',mergedData$MAJOR_DESC,ignore.case=TRUE) & mergedData$COURSE_ACRONYM == 'CSCI'] = 1
+mergedData$MAJOR_COURSE_SAME[grepl('mathematics',mergedData$MAJOR_DESC,ignore.case=TRUE) & mergedData$COURSE_ACRONYM == 'MATH'] = 1
+mergedData$MAJOR_COURSE_SAME[grepl('biology',mergedData$MAJOR_DESC,ignore.case=TRUE) & mergedData$COURSE_ACRONYM == 'BIOL'] = 1
+mergedData$MAJOR_COURSE_SAME[grepl('chemistry',mergedData$MAJOR_DESC,ignore.case=TRUE) & mergedData$COURSE_ACRONYM == 'CHEM'] = 1
+mergedData$MAJOR_COURSE_SAME[grepl('economics',mergedData$MAJOR_DESC,ignore.case=TRUE) & mergedData$COURSE_ACRONYM == 'ECON'] = 1
+mergedData$MAJOR_COURSE_SAME[grepl('environmental',mergedData$MAJOR_DESC,ignore.case=TRUE) & mergedData$COURSE_ACRONYM == 'ENSC'] = 1
+mergedData$MAJOR_COURSE_SAME[grepl('geography',mergedData$MAJOR_DESC,ignore.case=TRUE) & mergedData$COURSE_ACRONYM == 'GEOG'] = 1
+mergedData$MAJOR_COURSE_SAME[grepl('physics',mergedData$MAJOR_DESC,ignore.case=TRUE) & mergedData$COURSE_ACRONYM == 'PHYS'] = 1
+mergedData$MAJOR_COURSE_SAME[grepl('french',mergedData$MAJOR_DESC,ignore.case=TRUE) & mergedData$COURSE_ACRONYM == 'FREN'] = 1
+mergedData$MAJOR_COURSE_SAME[grepl('spanish',mergedData$MAJOR_DESC,ignore.case=TRUE) & mergedData$COURSE_ACRONYM == 'SPAN'] = 1
+mergedData$MAJOR_COURSE_SAME[grepl('psychology',mergedData$MAJOR_DESC,ignore.case=TRUE) & mergedData$COURSE_ACRONYM == 'PSYC'] = 1
+
+
+# MatchIt
+
+matchedFull <- matchit(
+    formula = treatment ~ 
+        INST_TERM_HRS_ATTEMPTED
+    + INST_CUM_GPA
+    + class_average
+    #+ INST_CUM_HRS_ATTEMPTED
+    + HS_GPA
+    + I(scale(SAT_CRITICAL_READING + SAT_MATH)) 
+    #+ BRIDGE_IND 
+    + GENDER_CODE
+    + IPEDS_RACE_CODE  
+    + NN_GRANT
+    + NR_GRANT
+    #+ STUDENT_OR_PARENT_AGI  
+    #+ MOTHER_HIGHEST_GRADE_CODE 
+    #+ FATHER_HIGHEST_GRADE_CODE 
+    #+ pop_over25_bachelors   
+    #+ income_household_median
+    + I(scale(home_medianValue))
+    + SUMMER_CNT
+    #+ MAJOR_COURSE_SAME
+    #+ ACTIVITY
+    #+ pop_armedForces  
+    #+ pop_over25_HSGrad
+    + AGE  
+    #+ MAJOR_CHANGES 
+    + pop  
+    + class_average
+    ,
+    distance="logit",
+    data=mergedData[,names(mergedData) %in% c('treatment','STU_INST_UID', 'INST_TERM_HRS_ATTEMPTED','GENDER_CODE','IPEDS_RACE_CODE','INST_CUM_GPA',
+                                              'NN_GRANT','NR_GRANT','home_medianValue','SUMMER_CNT','AGE','pop','class_average',
+                                              'MAJOR_IND','HS_GPA','SAT_CRITICAL_READING','SAT_MATH','INST_COURSE_GRADE',
+                                              'ACTIVITY','pop_armedForces','pop_over25_HSGrad','MAJOR_CHANGES','pop_over25_bachelors',
+                                              'BRIDGE_IND','STUDENT_OR_PARENT_AGI','MOTHER_HIGHEST_GRADE_CODE',
+                                              'FATHER_HIGHEST_GRADE_CODE','INST_CUM_HOURS_ATTEMPTED','TOTAL','MEAN','MEDIAN',
+                                              'MAJOR_COURSE_SAME','income_household_median','INST_CUM_HRS_ATTEMPTED')],
+    method='nearest',reestimate = TRUE
+)
+
+mergedData <- match.data(matchedFull)
+
+PSMData <- mergedData
+
+backupData <- mergedData
+
+# import datasets
+options(stringsAsFactors = FALSE) # don't coerce strings to factors
+lcdata <- read.csv(file="StudentData.csv", header=T)
+irdata <- read.csv(file="data_0708_1505.csv", header=T)
+sidata <- read.csv(file="SiData.csv", header=T)
+
+# drop transfer students
+ftf_ids = unique(irdata$STU_INST_UID[irdata$FTF_IPEDS_IND=='Y'])
+irdata <- irdata[irdata$STU_INST_UID %in% ftf_ids,]
+
+# remove duplicate rows (if any)
+irdata <- irdata[!duplicated(irdata),]
+
+# coerce names to lowercase for consistency
+irdata$FIRST_NAME <- tolower(irdata$FIRST_NAME)
+irdata$LAST_NAME <- tolower(irdata$LAST_NAME)
+lcdata$FIRST_NAME <- tolower(lcdata$FIRST_NAME)
+lcdata$LAST_NAME <- tolower(lcdata$LAST_NAME)
+
+# merge sets (left join)
+dataData <- merge(sidata, lcdata, by=c("TERM_CODE","COURSE_ACRONYM", 
+                                       "COURSE_NUMBER", "COURSE_SEC_IDENTIFIER", 
+                                       "SI_LEADER"), all.y=T, sort=F)
+
+mergedData <- merge(dataData, irdata, by=c("TERM_CODE","COURSE_ACRONYM", 
                                            "COURSE_NUMBER", "LAST_NAME", 
                                            "FIRST_NAME"), all.y=T, sort=F)
 
@@ -523,7 +859,13 @@ imputedData <- amelia(
           
            stemData <- imputedData$imputations[[1]]
   remove(imputedData)
-
+  
+  PSM_ids = unique(PSMData$STU_INST_UID)
+  
+  workData = stemData[stemData$STU_INST_UID %in% PSM_ids,]
+  
+  
+  
 #attach(stemData)
 #my.surv <- Surv(stemData$survTime, stemData$MAJOR_CHANGES)
 #coxph.fit <- coxph(my.surv ~ , method = "breslow", data=stemData)
